1052c348177af572d305c9f321cb6fa468a08e1e
Wonseok Lee <57432949+wonseok-lee@users.noreply.github.com>
2022-04-01 21:59:48 +0900

    Update 1149.py

--- BOJ/1149.py
+++ BOJ/1149.py
@@ -1,21 +1,13 @@
-n = int(input().strip())
-arr = [[0]*3]
-for _ in range(n) :
-    row = list(map(int, input().strip().split()))
-    arr.append(row)
+import sys
 
+input=sys.stdin.readline
 
-dp = [[0]*3 for _ in range(n+1)]
+n=int(input())
+matrix=[ list(map(int,input().split())) for _ in range(n)]
 
-for i in range(1, n+1) :
-    for j in range(3) :
-        if j == 0 :
-            dp[i][j] = min(dp[i-1][1], dp[i-1][2]) + arr[i][0]
+for i in range(1,n):
+    matrix[i][0]+=min(matrix[i-1][1],matrix[i-1][2])
+    matrix[i][1]+=min(matrix[i-1][0],matrix[i-1][2])
+    matrix[i][2]+=min(matrix[i-1][1],matrix[i-1][0])
 
-        elif j == 1 :
-            dp[i][j] = min(dp[i-1][0], dp[i-1][2]) + arr[i][1]
-
-        elif j == 2 :
-            dp[i][j] = min(dp[i-1][0], dp[i-1][1]) + arr[i][2]
-
-print(min(dp[n]))
+print(min(matrix[n-1]))
