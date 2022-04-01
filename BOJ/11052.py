40f6f3a50becf09ae14ec9736f1bc5ca524db594
Wonseok Lee <57432949+wonseok-lee@users.noreply.github.com>
2022-04-01 22:00:25 +0900

    Add files via upload

---
+++ BOJ/11052.py
@@ -0,0 +1,13 @@
+import sys
+
+input=sys.stdin.readline
+n=int(input())
+nums=[0]+list(map(int,input().split()))
+
+dp=[0 for _ in range(n+1)]
+
+for i in range(1,n+1):
+    for x in range(1,i+1):
+        dp[i]=max(dp[i],dp[i-x]+nums[x])
+
+print(dp[x])
