a0bbe7e8981ce956639825e083ef31f1411b2ceb
Wonseok Lee <57432949+wonseok-lee@users.noreply.github.com>
2022-04-16 22:45:28 +0900

    Update 16236.py

--- BOJ/16236.py
+++ BOJ/16236.py
@@ -1,4 +1,5 @@
 # 실패
+from re import L
 import sys
 from collections import deque
 
@@ -6,71 +7,80 @@ si=sys.stdin.readline
 N=int(si())
 grid=[list(map(int,si().split())) for _ in range(N)]
 
+start_x,start_y=-1,-1
+for i in range(N):
+    for j in range(N):
+        if grid[i][j]==9:
+            start_x,start_y=i,j
+            grid[i][j]=0
+
 visited=[[False for _ in range(N)] for _ in range(N)]
 step=[[0 for _ in range(N)] for _ in range(N)]
 dirs=[[0,1],[1,0],[0,-1],[-1,0]]
-answer=0
-time=0
-ate=False
 
-def bfs(x,y):
-    global answer, ate, time,visited
-    shark_init=2
+time=0
+ate=0
+shark_init=2
+
+def initialize_visited():
+    for i in range(N):
+        for j in range(N):
+            visited[i][j]=False
+
+def bfs():
+    global shark_init,ate,time
+    global start_x,start_y
+
+    initialize_visited()
+
     queue=deque()
-    queue.append((x,y))
-    visited[x][y]=True
-    eat=0
+    queue.append((start_x,start_y))
+
+    visited[start_x][start_y]=True
+    step[start_x][start_y]=0
 
     while queue:
-        food=deque(sorted(queue))
-        food_num=len(food)
+        x,y=queue.popleft()
 
-        for _ in range(food_num):
-            x,y=queue.popleft()
-            if grid[x][y]!=0 and grid[x][y]<shark_init:
-                grid[x][y]=0
-                eat+=1
+        for dx,dy in dirs:
+            nx,ny=x+dx,y+dy
+            if nx<0 or N<=nx or ny<0 or N<=ny:
+                continue
+            if visited[nx][ny]:
+                continue
+            if grid[nx][ny]>shark_init:
+                continue
+            queue.append((nx,ny))
+            visited[nx][ny]=True
+            step[nx][ny]=step[x][y]+1
+    
+    queue_new=deque()
+    queue_new.append((-1,-1))
+    for i in range(N):
+        for j in range(N):
+            if not visited[i][j] or grid[i][j]==shark_init or not grid[i][j]:
+                continue
+            queue_new.append((i,j))
+    
+    queue_new=deque(sorted(queue_new))
+    new_x,new_y=queue_new.popleft()
+    if new_x!=-1 and new_y!=-1:
+        time+=step[new_x][new_y]
+        grid[new_x][new_y]=0
+        start_x,start_y=new_x,new_y
+        ate+=1
 
-                if eat==shark_init:
-                    shark_init+=1
-                    eat=0
-            
-                queue=deque()
-                visited=[[False for _ in range(N)] for _ in range(N)]
-                visited[x][y]=True
-                ate=True
-                answer=time
+        if ate==shark_init:
+            ate=0
+            shark_init+=1
+        
+        return True
+    else:
+        return False
 
-            for dx,dy in dirs:
-                nx,ny=x+dx,y+dy
+while True:
+    bfs_res=bfs()
+    if not bfs_res:
+        break
 
-                if nx>=0 and N>nx and ny>=0 and N>ny and not visited[nx][ny]:
-                    if grid[nx][ny]<=shark_init:
-                    
-                        queue.append((nx,ny))
-                        visited[nx][ny]=True
-
-                # if nx<0 or N<=nx or ny<0 or N<=ny:
-                #     continue
-                # if visited[nx][ny]:
-                #     continue
-                # if grid[nx][ny]<=shark_init:
-                    
-                #     queue.append((nx,ny))
-                #     visited[nx][ny]=True
-            if ate:
-                ate=False
-                break
-
-        time+=1
-    return answer
-
-
-a, b = None, None
-for i in range(N):
-    for j in range(N):
-        if grid[i][j] == 9:
-            a, b = i, j
-            grid[i][j] = 0
-
-print(bfs(a,b))
+print(time)
