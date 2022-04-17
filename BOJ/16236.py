d070951ccc8a23d995863a9906f7b9974b90fbdc
Wonseok Lee <57432949+wonseok-lee@users.noreply.github.com>
2022-04-17 19:20:42 +0900

    Update 16236.py

--- BOJ/16236.py
+++ BOJ/16236.py
@@ -1,86 +1,52 @@
-# 실패
-from re import L
-import sys
 from collections import deque
-
-si=sys.stdin.readline
-N=int(si())
-grid=[list(map(int,si().split())) for _ in range(N)]
-
-start_x,start_y=-1,-1
-for i in range(N):
-    for j in range(N):
-        if grid[i][j]==9:
-            start_x,start_y=i,j
-            grid[i][j]=0
-
-visited=[[False for _ in range(N)] for _ in range(N)]
-step=[[0 for _ in range(N)] for _ in range(N)]
-dirs=[[0,1],[1,0],[0,-1],[-1,0]]
-
-time=0
-ate=0
-shark_init=2
-
-def initialize_visited():
-    for i in range(N):
-        for j in range(N):
-            visited[i][j]=False
-
-def bfs():
-    global shark_init,ate,time
-    global start_x,start_y
-
-    initialize_visited()
-
-    queue=deque()
-    queue.append((start_x,start_y))
-
-    visited[start_x][start_y]=True
-    step[start_x][start_y]=0
-
-    while queue:
-        x,y=queue.popleft()
-
-        for dx,dy in dirs:
-            nx,ny=x+dx,y+dy
-            if nx<0 or N<=nx or ny<0 or N<=ny:
-                continue
-            if visited[nx][ny]:
-                continue
-            if grid[nx][ny]>shark_init:
-                continue
-            queue.append((nx,ny))
-            visited[nx][ny]=True
-            step[nx][ny]=step[x][y]+1
-    
-    queue_new=deque()
-    queue_new.append((-1,-1))
-    for i in range(N):
-        for j in range(N):
-            if not visited[i][j] or grid[i][j]==shark_init or not grid[i][j]:
-                continue
-            queue_new.append((i,j))
-    
-    queue_new=deque(sorted(queue_new))
-    new_x,new_y=queue_new.popleft()
-    if new_x!=-1 and new_y!=-1:
-        time+=step[new_x][new_y]
-        grid[new_x][new_y]=0
-        start_x,start_y=new_x,new_y
-        ate+=1
-
-        if ate==shark_init:
-            ate=0
-            shark_init+=1
-        
-        return True
-    else:
-        return False
-
+import sys
+def bfs(i, j):
+    visit = [[0] * n for i in range(n)]
+    visit[i][j] = 1
+    eat = []
+    dist = [[0] * n for i in range(n)]
+    q = deque()
+    q.append([i, j])
+    while q:
+        x, y = q.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < n and 0 <= ny < n and visit[nx][ny] == 0:
+                if s[nx][ny] <= s[i][j] or s[nx][ny] == 0:
+                    q.append([nx, ny])
+                    visit[nx][ny] = 1
+                    dist[nx][ny] = dist[x][y] + 1
+                if s[nx][ny] < s[i][j] and s[nx][ny] != 0:
+                    eat.append([nx, ny, dist[nx][ny]])
+    if not eat:
+        return -1, -1, -1
+    eat.sort(key = lambda x : (x[2], x[0], x[1]))
+    return eat[0][0], eat[0][1], eat[0][2]
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+input = sys.stdin.readline
+n = int(input())
+s = []
+for i in range(n):
+    a = list(map(int, input().split()))
+    s.append(a)
+    for j in range(n):
+        if a[j] == 9:
+            s[i][j] = 2
+            start = [i, j]
+exp = 0
+cnt = 0
 while True:
-    bfs_res=bfs()
-    if not bfs_res:
-        break
-
-print(time)
+    i, j = start[0], start[1]
+    ex, ey, dist = bfs(i, j)
+    if ex == -1: break
+    s[ex][ey] = s[i][j]
+    s[i][j] = 0
+    start = [ex, ey]
+    exp += 1
+    if exp == s[ex][ey]:
+        exp = 0
+        s[ex][ey] += 1
+    cnt += dist
+print(cnt)
