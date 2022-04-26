d708af0dd0423c3a31f89aa703a22ac0d53ebb4e
Wonseok Lee <57432949+wonseok-lee@users.noreply.github.com>
2022-04-26 21:35:19 +0900

    upload

---
+++ programmers/N으로 표현.py
@@ -0,0 +1,23 @@
+answer = -1
+
+def DFS(n, pos, num, number, s):
+    global answer
+    if pos > 8:
+        return
+    if num == number:
+        if pos < answer or answer == -1:
+            #print(s)            
+            answer=pos
+        return
+
+    nn=0
+    for i in range(8):
+        nn=nn*10+n
+        DFS(n, pos+1+i, num+nn, number, s+'+')
+        DFS(n, pos+1+i, num-nn, number, s+'-')
+        DFS(n, pos+1+i, num*nn, number, s+'*')
+        DFS(n, pos+1+i, num/nn, number, s+'/')
+
+def solution(N, number):    
+    DFS(N, 0, 0, number, '')    
+    return answer
